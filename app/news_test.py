940094083f6bbb3735d005d8a0a6db38ced99d96
annechege <annechege780@gmail.com>
2021-06-24 15:23:28 +0300

    adding photo

--- app/news_test.py
+++ app/news_test.py
@@ -11,7 +11,7 @@ def setUp(self):
         '''
         Set up method that will run before every Test
         '''
-        self.new_new = News1234,'Python Must Be Crazy','A thrilling new Python Series','https://newsapi.org/v2/top-headlines?country=us&apiKey',8.5,129993)
+        self.new_new = News1234,'Python Must Be Crazy','A thrilling new Python Series',('https://newsapi.org/v2/top-headlines?country=us&apiKey',8.5,129993)
 
 def test_instance(self):
         self.assertTrue(isinstance(self.new_news,News))
